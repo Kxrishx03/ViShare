20e98d620b92994f91578114e473b387f85d623f
Kxrishx03 <2001karishmamandal@gmail.com>
2024-07-01 17:55:37 +0530

    """"

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,22 @@ const PORT = process.env.PORT;
 const cors = require("cors");
 app.use(cors());
 
+
+//CORS error
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+//     // Handle preflight requests
+//     if (req.method === 'OPTIONS') {
+//         res.sendStatus(200);
+//     } else {
+//         next();
+//     }
+// });
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI,{
     dbName: 'vishareDB',
